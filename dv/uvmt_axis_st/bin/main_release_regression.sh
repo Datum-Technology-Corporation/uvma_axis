3796c3663e551f08f51c2361679b409cfed7c40b
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:47:51 -0400

    updated regression scripts for new CLI

--- dv/uvmt_axis_st/bin/main_release_regression.sh
+++ dv/uvmt_axis_st/bin/main_release_regression.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 ########################################################################################################################
-## Copyright 2021 Datum Technology Corporation
+## Copyright 2021-2022 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
 
 
-# Launched from uvml project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_axis_st
-mio sim     uvmt_axis_st -t rand_traffic -s 1 -c
+# Launched from uvma_axis project sim dir
+mio sim     uvmt_axis_st -CE
+mio sim     uvmt_axis_st -S -t rand_traffic -s 1 -c
 mio results uvmt_axis_st results
 mio cov     uvmt_axis_st
